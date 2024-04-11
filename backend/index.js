51aa4da4986262f20a8872aa2ec27b28fcb0cf26
VictordelaM <delamotte.v@gmail.com>
2024-04-11 14:19:50 +0200

    index backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("victor ist cool schwöre möhre")
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import "dotenv/config"
+import {userRouter} from './controller/user.js'
+import {imageRouter} from './controller/image.js'
+
+
+const PORT = 3000
+const app = express()
+
+await mongoose.connect(process.env.MONGODB_URI)
+
+app.use(cors())
+app.use("/user", userRouter)
+app.use("/image", imageRouter)
+
+app.listen(PORT)
+console.log(`listen http://localhost:${PORT}`)
