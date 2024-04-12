5bf17a69304ae1c7647e890be224591f30b3904c
Bwalyaa <bwalya@hotmail.de>
2024-04-12 15:42:18 +0200

    upload form

---
+++ frontend/src/pages/UploadImg.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+import UploadImgForm from "../components/UploadImgForm";
+
+const UploadImg = () => {
+    return ( 
+        <>
+        <Nav />
+        <UploadImgForm />
+        </>
+     );
+}
+ 
+export default UploadImg;
